6d49e7050bfb54de32de797df5799d66168757da
chrisjamesr <chrisjames.r@gmail.com>
2020-03-25 12:24:54 -0400

    relocate and delete files

--- add.spec.js
+++
@@ -1,9 +0,0 @@
-// Simple testing library example
-const {add} = require('./index');
-const {test, assert} = require('./lib');
-
-test('the add funciton adds two numbers', function() {
-    const actual = add(3,4);
-    const expected = 9;
-    assert.equal(actual, expected);
-})
