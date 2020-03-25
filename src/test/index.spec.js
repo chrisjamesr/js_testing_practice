45c2c599dcc2a6ed6ceeb100303ac70e0f87b4a8
chrisjamesr <chrisjames.r@gmail.com>
2020-03-25 12:24:24 -0400

    create new file structure. move necessary files to new locations

---
+++ src/test/index.spec.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const { add } = require('./index');
+
+describe("The 'add' function tests", function(){
+
+    it('Should return 11 for arguments 5 and 6', function (){
+        
+        const actual = add(5, 6);
+        const expected = 11;
+
+        assert.equal(actual, expected);
+
+    })
+
+    it('Should throw an error if not given arguments', function () {
+        
+        assert.throws( () => {
+            add(); 
+        });
+
+    })
+})
