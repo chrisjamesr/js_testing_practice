3921f2ea1485455eb59620d30d4fec099d951031
chrisjamesr <chrisjames.r@gmail.com>
2020-03-25 17:48:09 -0400

    add test for error on user.id not found

--- src/test/async.spec.js
+++ src/test/async.spec.js
@@ -4,37 +4,42 @@ const { findUserById, findUserByEmail } = require('../async');
 describe('the async tests', () => {
 
     describe('the findUserById function', () => {
-        it.only('should return found user data by id & checks promise is resolved correctly', () => {
+        it('should return found user data by id & checks promise is resolved correctly', () => {
             // returns promise -> checks for data errors and avoids unhandled promise rejection errors associated w/done function
             return findUserById(1).then(result => {
                 
-                console.log('@@ ===> ', result);
-
                 assert.equal(result.user.name, 'chris');
 
             });
         });
 
-        it.only('should return found user data by id', (done) => {
+        it('should return found user data by id', (done) => {
             // calls done on promise return 
             findUserById(1).then(result => {
-                
-                console.log('@@ ===> done....', result);
-
+            
                 assert.equal(result.user.name, 'chris');
 
                 done()
 
             });
         });
+
+        it('should throw an error if user was not found', () => {
+            return findUserById(1000).catch(error => {
+            
+                assert.equal(error.message, `User with id: 1000 was not found`)
+       
+            });
+        });
     });
 
     describe('the findUserByEmail function', () => {
-        it.only('should return found user data by email', () => {
+        it('should return found user data by email', () => {
 
             return findUserByEmail('zack@email.com').then(result => {
-                console.log('working...done...', result);
+                
                 assert.equal(result.user.name, 'zack');
+
             })
         })
     });
