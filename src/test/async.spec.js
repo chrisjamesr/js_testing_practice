28af65480d9783ff32bc9b0309c587e67040adfa
chrisjamesr <chrisjames.r@gmail.com>
2020-03-25 17:12:07 -0400

    add test for findUserById data and promise handling

---
+++ src/test/async.spec.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const { findUserById, findUserByEmail } = require('../async');
+
+describe('the async tests', () => {
+
+    describe('the findUserById function', () => {
+        it.only('should return found user data by id & checks promise is resolved correctly', () => {
+            // returns promise -> checks for data errors and avoids unhandled promise rejection errors associated w/done function
+            return findUserById(1).then(result => {
+                
+                console.log('@@ ===> ', result);
+
+                assert.equal(result.user.name, 'chris');
+
+            });
+        });
+        
+        it.only('should return found user data by id', (done) => {
+            // calls done on promise return 
+            findUserById(1).then(result => {
+                
+                console.log('@@ ===> done....', result);
+
+                assert.equal(result.user.name, 'chris');
+
+                done()
+
+            });
+        });
+    });
+});
